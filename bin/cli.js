4f5f6cd7eccacfabea5c4c682edc907b342f7b88
Maurice Prosper <maurice.prosper@ttu.edu>
2016-04-20 18:21:16 -0500

    command line doesn't spawn children to create range

--- bin/cli.js
+++ bin/cli.js
@@ -8,44 +8,35 @@ program
     .version(package.version)
     .usage('[options] <file ...>')
     .option('-s, --window-size <int>', 'Size of the Sliding window', function(a){ return parseInt(a, 10); }, 3)
-    .option('-o, --output <file>', 'Where to save the Sliding output, use % to replace with window size')
-    .option('-r, --range <a>..<b>', 'A range of windows', function range(val) { return val.split('..').map(Number); })
+    .option('-o, --output <file>', 'Path to save the sliding output [use % to replace with window size]')
+    .option('-i, --input <file>', 'Path to read the data stream')
+    .option('-r, --range <a>..<b>', 'A range of windows', function (val) { return val.split('..').map(Number); })
     .option('--one-hot', 'Make the label a one hot vector')
     .parse(process.argv);
 program.range = program.range || [];
 
+// turn one size into a 'range'
+if(program.windowSize)
+    program.range[0] = program.range[1] = program.windowSize;
+
 // program for all numbers in range
-if(program.range.length) {
-    const spawn = require('child_process');
-
-    for(var ws = program.range[0]; ws <= program.range[1]; ws++) {
-        cli = spawn.execSync('node bin\\cli.js ' + [
-                '-s', ws,
-                '-o', program.output,
-                program.args.join(' ')
-            ].join(' '));
-
-        console.log('Running with window size %d\n\t%s', ws, cli);
-    }
-} else {
+for(var ws = program.range[0]; ws <= program.range[1]; ws++) {
     // make Sliding window on all files
-    var outFile = '';
     if (program.output)
         outFile = program.output.replace('%', program.windowSize);
-    program.args.forEach(function (v) {
-        fs.readFile(v, function (err, data) {
-            if (err) throw err;
 
-            // output
-            var output = sliding(data, program.windowSize, program.oneHot);
+    fs.readFile(program.input, function (err, data) {
+        if (err) throw err;
 
-            if (program.output)
-                fs.writeFile(outFile, JSON.stringify(output), function (err, ret) {
-                    if (err) throw err;
-                    console.log('Sliding Window of %d files written to %s.', program.args.length, outFile);
-                });
-            else
-                console.log(output);
-        });
+        // output
+        var output = sliding(data, program.windowSize, program.oneHot);
+
+        if (program.output)
+            fs.writeFile(outFile, JSON.stringify(output), function (err, ret) {
+                if (err) throw err;
+                console.log('Sliding window [%d] "%s" -> "%s"', ws, program.input, outFile);
+            });
+        else
+            console.log(output);
     });
 }
