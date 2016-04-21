b24a1360b363c856f5c2501d9fac9ff2fcec9cc6
Maurice Prosper <maurice.prosper@ttu.edu>
2016-04-21 16:41:20 -0500

    Fixed window sizing

--- bin/builder.js
+++ bin/builder.js
@@ -9,15 +9,15 @@ program
     .description(pkg.description)
     .usage('[options] <file ...>')
     .option('-s, --window-size <int>', 'size of the Sliding window', function(a){ return parseInt(a, 10); })
+    .option('-r, --range <a>..<b>', 'a range of windows', function (val) { return val.split('..').map(Number); }, [])
     .option('-o, --output <file>', 'path to save the sliding output [use % to replace with window size]')
     .option('-i, --input <file>', 'path to read the data stream')
-    .option('-r, --range <a>..<b>', 'a range of windows', function (val) { return val.split('..').map(Number); })
     .option('--one-hot', 'make the label a one hot vector')
     .parse(process.argv);
 
 // turn one size into a 'range'
-if(program.windowOffset)
-    program.range[0] = program.range[1] = program.windowOffset;
+if(program.windowSize)
+    program.range = [program.windowSize, program.windowSize];
 
 // we need a window size
 // program.range = program.range || [];
@@ -28,7 +28,7 @@ if(program.range.length !== 2 || !program.input)
 for(var ws = program.range[0]; ws <= program.range[1]; ws++) {
     // make Sliding window on all files
     if (program.output)
-        outFile = program.output.replace('%', ws);
+        var outFile = program.output.replace('%', ws);
 
     var data = fs.readFileSync(program.input),
         output = sliding(data, ws, program.oneHot);
